c56e34d9306f730d260e7f741fb582db94309a3d
jjhkie <jjhkie0916@naver.com>
2022-06-11 19:39:41 +0900

    #8 detail Page image 처리 및 CarouselSlider를 활용하여 슬라이드 기능 추가

--- lib/page/detail.dart
+++ lib/page/detail.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class DetailContentView extends StatefulWidget {
@@ -11,10 +12,21 @@ class DetailContentView extends StatefulWidget {
 
 class _DetailContentViewState extends State<DetailContentView> {
   late Size size;
+  late List<Map<String,dynamic>> imgList;
+  late int _current;
+
   @override
   void didChangeDependencies(){
     super.didChangeDependencies();
     size = MediaQuery.of(context).size;
+    imgList = [
+      {"id":"0","url":widget.data['image']},
+      {"id":"1","url":widget.data['image']},
+      {"id":"2","url":widget.data['image']},
+      {"id":"3","url":widget.data['image']},
+      {"id":"4","url":widget.data['image']},
+    ];
+    _current = 0;
   }
 
   @override
@@ -23,6 +35,7 @@ class _DetailContentViewState extends State<DetailContentView> {
       extendBodyBehindAppBar: true,
       appBar: _appBarWidget(),
       body: _bodyWidget(),
+      bottomNavigationBar: _bottomBarWidget(),
     );
   }
 
@@ -30,26 +43,93 @@ class _DetailContentViewState extends State<DetailContentView> {
     return AppBar(
       backgroundColor: Colors.transparent, //부모의 색을 따라간다 - 투명
       elevation: 0,
+      leading: IconButton(onPressed:(){
+        Navigator.pop(context);
+      },icon: Icon(Icons.arrow_back)),
       actions: [
         IconButton(
           onPressed: () {},
-          icon: Icon(Icons.share),
-          color: Colors.black,
+          icon: Icon(Icons.share)
         ),
         IconButton(
-            onPressed: () {}, icon: Icon(Icons.more_vert), color: Colors.black)
+            onPressed: () {}, icon: Icon(Icons.more_vert))
       ],
     );
   }
 
   Widget _bodyWidget() {
+    return Column(
+      children: [
+        _makeSliderImage(),
+
+      ],
+    );
+  }
+
+  Widget _bottomBarWidget(){
     return Container(
-      child: Hero(
-        tag: widget.data['cid'],
-        child: Image.asset(widget.data['image'],
-            width: size.width,
-            fit: BoxFit.fill),
+      width: size.width,
+      height: 55,
+      color: Colors.blueAccent,
+    );
+  }
+
+  Widget _makeSliderImage(){
+    return Container(
+      child: Stack(
+        children: [
+          Hero(
+              tag: widget.data['cid'],
+              child: CarouselSlider(
+                  items: imgList.map((map){
+                    return Image.asset(map["url"],
+                        width: size.width,
+                        fit: BoxFit.fill);
+                  }).toList(),
+                  options: CarouselOptions(
+                      height: size.width,
+                      initialPage: 0,
+                      enableInfiniteScroll: false,
+                      viewportFraction: 1,//화면 사용 비율
+                      onPageChanged: (index,reason){
+                        setState((){
+                          _current = index;
+                        });
+                      }
+                  )
+
+              )
+
+
+          ),
+          Positioned(
+            bottom:0,
+            left:0,
+            right: 0,
+
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: imgList.asMap().entries.map((entry) {
+                return GestureDetector(
+                  onTap: () => {},
+                  child: Container(
+                    width: 8.0,
+                    height: 8.0,
+                    margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: (Theme.of(context).brightness == Brightness.dark
+                            ? Colors.white
+                            : Colors.black)
+                            .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        ],
       ),
     );
   }
+
 }
